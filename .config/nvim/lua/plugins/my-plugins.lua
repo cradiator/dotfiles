7aa85cac9c7345283a15b9cd53075784cf7bec49
crady <crady@cradys-MacBook-Pro.local>
2024-12-07 10:30:27 -0800

    Add vim-tmux-navigator

--- .config/nvim/lua/plugins/my-plugins.lua
+++ .config/nvim/lua/plugins/my-plugins.lua
@@ -1,3 +1,11 @@
 return {
-  { "mg979/vim-visual-multi", vscode = true },
+  { "mg979/vim-visual-multi" },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+    end,
+  },
 }
