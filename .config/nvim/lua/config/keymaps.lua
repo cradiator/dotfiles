517724ee5c9ea9fd580659bf540bdeafb914b250
crady <crady@cradys-MacBook-Pro.local>
2024-12-10 08:24:35 -0800

    nvim ctrl+o == new line in insert mode add tmux-yank to gitignore

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,5 @@ vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
 vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+
+vim.keymap.set("i", "<C-o>", "<Esc>o", { noremap = true, silent = true })
