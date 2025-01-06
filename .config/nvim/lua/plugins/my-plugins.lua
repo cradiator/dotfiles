c6eebd9912342ea99bcad8761a2c42dd372409f9
xnzhao <xnzhao@google.com>
2025-01-06 22:38:55 +0000

    support OSC52 in nvim and tmux

--- .config/nvim/lua/plugins/my-plugins.lua
+++ .config/nvim/lua/plugins/my-plugins.lua
@@ -8,4 +8,17 @@ return {
       })
     end,
   },
+  {
+    "ojroques/nvim-osc52",
+    config = function()
+      require("osc52").setup({
+        copy_register = '"+',
+      })
+
+      local map = vim.keymap.set
+      map("n", "<leader>y", require("osc52").copy_operator, { expr = true, desc = "Copy with OSC52" })
+      map("n", "<leader>yy", "<leader>y_", { remap = true, desc = "Copy line with OSC52" })
+      map("v", "<leader>y", require("osc52").copy_visual, { desc = "Copy visual selection with OSC52" })
+    end,
+  },
 }
