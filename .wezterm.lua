43dbfa1ce46e29b73716331ca0887ddc3b5d4a9b
crady <craditor@gmail.com>
2025-03-03 19:26:00 -0800

    wezterm use fish shell if exist

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.font = wezterm.font("0xProto Nerd Font Mono")
 config.font_size = 13
+
+if io.open("/opt/homebrew/bin/fish", "r") then
+  config.default_prog = {"/opt/homebrew/bin/fish"}
+end
+
 -- and finally, return the configuration to wezterm
 return config
