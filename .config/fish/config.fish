62fc3f15ed8d93190ae8e70719d0af3f9910b95c
crady <craditor@gmail.com>
2025-03-04 06:06:40 -0800

    run homebrew.fish first before other conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,4 @@ if status is-interactive
 end
 
 # Check if ~/.local/bin exists and add it to PATH if not already included
-if test -d "$HOME/.local/bin"
-    if not contains "$HOME/.local/bin" $PATH
-        set -p PATH "$HOME/.local/bin"
-    end
-end
+test -d "$HOME/.local/bin"; and set -p PATH "$HOME/.local/bin"
